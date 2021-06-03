799698c2a60762bca0639a69d7c570e0d9ae938a
PauloFranco <paulimsantos@gmail.com>
2021-06-03 13:13:06 -0300

    jogador linha no card

--- resources/views/games/partials/card-content.blade.php
+++ resources/views/games/partials/card-content.blade.php
@@ -6,6 +6,8 @@
         <small class="pad-right-15 no-padding--empty">{{$user->name}}</small>
         @if($user->goalkeeper)
             <small class="pad-right-15"> - goleiro</small>
+        @else
+            <small class="pad-right-15"> - linha</small>
         @endif
     </p>
     @endforeach
